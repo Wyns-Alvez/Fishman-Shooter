1ba082f14747337e1b48cc0cd7bd7acc466b5cbb
Wyns-Alvez <alvez.046036@balayan.sti.edu.ph>
2022-04-09 13:21:11 +0800

    add shooting function and enemy  animation

--- game.py
+++ game.py
@@ -10,14 +10,24 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Fishman Shooter')
 
+#set framerate
 clock = pygame.time.Clock()
 FPS = 60
 
+#define game variables
 GRAVITY = 0.75
 
+#define player action variables
 moving_left = False
 moving_right = False
+shoot = False
 
+#load images
+#bullet
+bullet_img = pygame.image.load('assets/bullet/bullet.png').convert_alpha()
+
+
+#define colours
 BG = (144, 201, 120)
 RED = (255, 0, 0)
 
@@ -26,10 +36,16 @@ def draw_bg():
     pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
 
 class Fishman(pygame.sprite.Sprite):
-    def __init__(self, char_type, x, y, scale, speed):
+    def __init__(self, char_type, x, y, scale, speed, ammo):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
         self.char_type = char_type
         self.speed = speed
+        self.ammo = ammo
+        self.start_ammo = ammo
+        self.shoot_cooldown = 0
+        self.health = 100
+        self.max_health = self.health
         self.direction = 1
         self.vel_y = 0
         self.jump = False
@@ -39,10 +55,13 @@ class Fishman(pygame.sprite.Sprite):
         self.frame_index = 0
         self.action = 0
         self.update_time = pygame.time.get_ticks()
-        
-        animation_types = ['idle', 'walk', 'jump']
+
+        #load all images for the players
+        animation_types = ['idle', 'walk', 'jump', 'death','attack']
         for animation in animation_types:
+            #reset temporary list of images
             temp_list = []
+            #count number of files in the folder
             num_of_frames = len(os.listdir(f'assets/{self.char_type}/{animation}'))
             for i in range(num_of_frames):
                 img = pygame.image.load(f'assets/{self.char_type}/{animation}/{i}.png')
@@ -54,10 +73,19 @@ class Fishman(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
+    def update(self):
+        self.update_animation()
+        self.check_alive()
+        #update cooldown
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
+
     def move(self, moving_left, moving_right):
+        #reset movement variables
         dx = 0
         dy = 0
 
+        #assign movement variables if moving left or right
         if moving_left:
             dx = -self.speed
             self.flip = True
@@ -66,6 +94,7 @@ class Fishman(pygame.sprite.Sprite):
             dx = self.speed
             self.flip = False
             self.direction = 1
+
          #jump
         if self.jump == True and self.in_air == False:
             self.vel_y = -11
@@ -83,10 +112,21 @@ class Fishman(pygame.sprite.Sprite):
             dy = 300 - self.rect.bottom
             self.in_air = False
 
+
+        #update rectangle position
         self.rect.x += dx
         self.rect.y += dy
 
+    def shoot(self):
+        if self.shoot_cooldown == 0 and self.ammo > 0:
+            self.shoot_cooldown = 20
+            bullet = Bullet(self.rect.centerx + (0.6 * self.rect.size[0] * self.direction), self.rect.centery, self.direction)
+            bullet_group.add(bullet)
+            #reduce ammo
+            self.ammo -= 1
+
     def update_animation(self):
+
         #update animation
         ANIMATION_COOLDOWN = 100
         #update image depending on current frame
@@ -97,7 +137,10 @@ class Fishman(pygame.sprite.Sprite):
             self.frame_index += 1
         #if the animation has run out the reset back to the start
         if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
+            if self.action == 3:
+                self.frame_index = len(self.animation_list[self.action]) - 1
+            else:
+                self.frame_index = 0
 
 
 
@@ -109,14 +152,28 @@ class Fishman(pygame.sprite.Sprite):
             self.frame_index = 0
             self.update_time = pygame.time.get_ticks()
 
+    def check_alive(self):
+        if self.health <= 0:
+            self.health = 0
+            self.speed = 0
+            self.alive = False
+            self.update_action(3)
+
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, char_type, x, y, scale, speed):
+    def __init__(self, char_type, x, y, scale, speed, ammo):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
         self.char_type = char_type
         self.speed = speed
+        self.ammo = ammo
+        self.start_ammo = ammo
+        self.shoot_cooldown = 0
+        self.health = 100
+        self.max_health = self.health
         self.direction = 1
         self.vel_y = 0
         self.jump = False
@@ -126,10 +183,13 @@ class Enemy(pygame.sprite.Sprite):
         self.frame_index = 0
         self.action = 0
         self.update_time = pygame.time.get_ticks()
-        
-        animation_types = ['idle', 'walk']
+
+        #load all images for the players
+        animation_types = ['idle', 'death']
         for animation in animation_types:
+            #reset temporary list of images
             temp_list = []
+            #count number of files in the folder
             num_of_frames = len(os.listdir(f'assets/{self.char_type}/{animation}'))
             for i in range(num_of_frames):
                 img = pygame.image.load(f'assets/{self.char_type}/{animation}/{i}.png')
@@ -141,10 +201,19 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
+    def update(self):
+        self.update_animation()
+        self.check_alive()
+        #update cooldown
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
+
     def move(self, moving_left, moving_right):
+        #reset movement variables
         dx = 0
         dy = 0
 
+        #assign movement variables if moving left or right
         if moving_left:
             dx = -self.speed
             self.flip = True
@@ -153,6 +222,7 @@ class Enemy(pygame.sprite.Sprite):
             dx = self.speed
             self.flip = False
             self.direction = 1
+
          #jump
         if self.jump == True and self.in_air == False:
             self.vel_y = -11
@@ -170,10 +240,21 @@ class Enemy(pygame.sprite.Sprite):
             dy = 300 - self.rect.bottom
             self.in_air = False
 
+
+        #update rectangle position
         self.rect.x += dx
         self.rect.y += dy
 
+    def shoot(self):
+        if self.shoot_cooldown == 0 and self.ammo > 0:
+            self.shoot_cooldown = 20
+            bullet = Bullet(self.rect.centerx + (0.6 * self.rect.size[0] * self.direction), self.rect.centery, self.direction)
+            bullet_group.add(bullet)
+            #reduce ammo
+            self.ammo -= 1
+
     def update_animation(self):
+
         #update animation
         ANIMATION_COOLDOWN = 100
         #update image depending on current frame
@@ -184,7 +265,10 @@ class Enemy(pygame.sprite.Sprite):
             self.frame_index += 1
         #if the animation has run out the reset back to the start
         if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
+            if self.action == 1:
+                self.frame_index = len(self.animation_list[self.action]) - 1
+            else:
+                self.frame_index = 0
 
 
 
@@ -196,11 +280,52 @@ class Enemy(pygame.sprite.Sprite):
             self.frame_index = 0
             self.update_time = pygame.time.get_ticks()
 
-    def draw(self):
-        screen.blit(self.image, self.rect)
+    def check_alive(self):
+        if self.health <= 0:
+            self.health = 0
+            self.speed = 0
+            self.alive = False
+            self.update_action(1)
 
-player = Fishman('player',200, 200, 3, 5)
-enemy = Enemy('enemy',400, 200, 3, 5)
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+class Bullet(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 10
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+
+    def update(self):
+        #move bullet
+        self.rect.x += (self.direction * self.speed)
+        #check if bullet has gone off screen
+        if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
+            self.kill()
+
+        #check collision with characters
+        if pygame.sprite.spritecollide(player, bullet_group, False):
+            if player.alive:
+                player.health -= 5
+                self.kill()
+        if pygame.sprite.spritecollide(enemy, bullet_group, False):
+            if enemy.alive:
+                enemy.health -= 25
+                self.kill()
+
+
+
+#create sprite groups
+bullet_group = pygame.sprite.Group()
+
+player = Fishman('player',200, 200, 1, 5, 20)
+
+enemy = Enemy('enemy',200, 200, 1, 5, 20)
+
 
 
 
@@ -211,14 +336,24 @@ while run:
 
     draw_bg()
 
-    player.update_animation()
+    player.update()
     player.draw()
+
+    enemy.update()
     enemy.draw()
 
-     #update player actions
+    #update and draw groups
+    bullet_group.update()
+    bullet_group.draw(screen)
+
+    #update player actions
     if player.alive:
+        if shoot:
+            player.shoot()
         if player.in_air:
             player.update_action(2)#2: jump
+        elif shoot:
+            player.update_action(4)#4: shoot
         elif moving_left or moving_right:
             player.update_action(1)#1: run
         else:
@@ -235,10 +370,13 @@ while run:
                 moving_left = True
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_SPACE:
+                shoot = True
             if event.key == pygame.K_w and player.alive:
                 player.jump = True
             if event.key == pygame.K_ESCAPE:
                 run = False
+
 
         #keyboard button released
         if event.type == pygame.KEYUP:
@@ -246,7 +384,8 @@ while run:
                 moving_left = False
             if event.key == pygame.K_d:
                 moving_right = False
-
+            if event.key == pygame.K_SPACE:
+                shoot = False
 
 
 
